a36dcd8c2093c1f605beb83e33c9caf06ef7ee04
Sheikh Hashir Ahmed <ahmedhashir845@gmail.com>
2023-01-15 00:14:09 +0500

    initial commmit. Recommendations

---
+++ jsrec/recommend.js
@@ -0,0 +1,37 @@
+const fetch = require('node-fetch');
+
+
+class Song {
+    constructor(song){
+        this.id = song['id']
+        this.name = song['name']
+        this.album = song['album']
+        this.artist = song['primaryArtists'].split(",")
+        this.download_url = song['downloadUrl'][4]['link']
+        this.label = song['label']
+        this.play_count = song['playCount']
+
+        
+    }
+
+    get_recommendations(){
+        for(let item of this.artist){
+           fetch("http://saavn.me/search/songs?query="+item) //---------------------
+  .then(res => res.json()).then(json => console.log(json['data']['results']))
+        }
+    }
+
+
+  }
+// query in the url to be replaced by variable based on search ---------------------------------------- 
+
+let q = "tukur tukur";
+fetch("http://saavn.me/search/songs?query="+q) //---------------------
+  .then(res => res.json())
+  .then(
+    json => song = new Song(json['data']['results'][0])) // ek song k liye 0th index------------ list of songs k lye loop se
+    .then(
+    song => (song.get_recommendations()))
+
+
+  
