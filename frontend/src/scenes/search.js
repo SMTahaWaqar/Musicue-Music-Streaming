afc719abfc317bcf78b4a44f9435fc853ab1c7ee
SMTahaWaqar <syedtaha.hsn@gmail.com>
2023-01-16 00:29:27 +0500

    Implemnted Search Screen

--- frontend/src/scenes/search.js
+++ frontend/src/scenes/search.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react'
 import { Link, useLocation } from 'react-router-dom';
+import {BsFillHeartFill} from 'react-icons/bs';
 
 const Search = () => {
 
@@ -46,11 +47,11 @@ const Search = () => {
                   <p className="text-gray-700 text-base mb-4">
                     {result.type}
                   </p>
-                  {/* <div className='flex justify-between flex-col'>
+                  <div className='flex justify-between flex-col'>
                     <button type="button" className=" inline-block px-6 py-2.5 bg-black text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-[#EA0C5C] hover:shadow-lg focus:bg-[#EA0C5C] focus:shadow-lg focus:outline-none focus:ring-0 active:bg-[#EA0C5C] active:shadow-lg transition duration-150 ease-in-out my-2 hover:scale-110">Play</button>
                     <button type="button" className=" inline-block px-6 py-2.5 bg-black text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-[#EA0C5C] hover:shadow-lg focus:bg-[#EA0C5C] focus:shadow-lg focus:outline-none focus:ring-0 active:bg-[#EA0C5C] active:shadow-lg transition duration-150 ease-in-out my-2 hover:scale-110">Add to Playlist</button>
                     <button type="button" className=" inline-block px-6 py-2.5 bg-black text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-[#EA0C5C] hover:shadow-lg focus:bg-[#EA0C5C] focus:shadow-lg focus:outline-none focus:ring-0 active:bg-[#EA0C5C] active:shadow-lg transition duration-150 ease-in-out my-2 hover:scale-110 flex items-center justify-center"><BsFillHeartFill /></button>
-                  </div> */}
+                  </div>
                 </div>
               </div>
             </div>
@@ -58,6 +59,91 @@ const Search = () => {
           </div>
           : <div>Loading</div>
           }
+
+          <h4 className="text-xl md:text-lg text-indigo-700 mt-10">Songs</h4>
+          {searchData ?
+          <div className='flex flex-wrap items-centerz justify-center md:grid grid-cols-4 gap-4 items-stretch'>
+            {searchData.songs.results.map((result) => (
+              <div className="flex justify-center m-4 w-full h-full z-[1]">
+              <div className="rounded-lg shadow-lg bg-white max-w-sm">
+                <Link to={result.type === "song" ? "/song" : "/album"} state={result.title} data-mdb-ripple="true" data-mdb-ripple-color="light">
+                  <img className="rounded-t-lg" src={result.image[2].link} alt="Song Cover"/>
+                </Link>
+                <div className="p-6">
+                  <h5 className="text-gray-900 text-xl font-medium mb-2">{result.title}</h5>
+                  <p className="text-gray-700 text-base mb-4">
+                    {result.type}
+                  </p>
+                  <div className='flex justify-between flex-col'>
+                    <button type="button" className=" inline-block px-6 py-2.5 bg-black text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-[#EA0C5C] hover:shadow-lg focus:bg-[#EA0C5C] focus:shadow-lg focus:outline-none focus:ring-0 active:bg-[#EA0C5C] active:shadow-lg transition duration-150 ease-in-out my-2 hover:scale-110">Play</button>
+                    <button type="button" className=" inline-block px-6 py-2.5 bg-black text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-[#EA0C5C] hover:shadow-lg focus:bg-[#EA0C5C] focus:shadow-lg focus:outline-none focus:ring-0 active:bg-[#EA0C5C] active:shadow-lg transition duration-150 ease-in-out my-2 hover:scale-110">Add to Playlist</button>
+                    <button type="button" className=" inline-block px-6 py-2.5 bg-black text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-[#EA0C5C] hover:shadow-lg focus:bg-[#EA0C5C] focus:shadow-lg focus:outline-none focus:ring-0 active:bg-[#EA0C5C] active:shadow-lg transition duration-150 ease-in-out my-2 hover:scale-110 flex items-center justify-center"><BsFillHeartFill /></button>
+                  </div>
+                </div>
+              </div>
+            </div>
+            ))}
+          </div>
+          : <div>Loading</div>
+          }
+
+
+          <h4 className="text-xl md:text-lg text-indigo-700 mt-10">Albums</h4>
+          {searchData ?
+          <div className='flex flex-wrap items-centerz justify-center md:grid grid-cols-4 gap-4 items-stretch'>
+            {searchData.albums.results.map((result) => (
+              <div className="flex justify-center m-4 w-full h-full z-[1]">
+              <div className="rounded-lg shadow-lg bg-white max-w-sm">
+                <Link to={result.type === "song" ? "/song" : "/album"} state={result.title} data-mdb-ripple="true" data-mdb-ripple-color="light">
+                  <img className="rounded-t-lg" src={result.image[2].link} alt="Song Cover"/>
+                </Link>
+                <div className="p-6">
+                  <h5 className="text-gray-900 text-xl font-medium mb-2">{result.title}</h5>
+                  <p className="text-gray-700 text-base mb-4">
+                    {result.type}
+                  </p>
+                  <div className='flex justify-between flex-col'>
+                    <button type="button" className=" inline-block px-6 py-2.5 bg-black text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-[#EA0C5C] hover:shadow-lg focus:bg-[#EA0C5C] focus:shadow-lg focus:outline-none focus:ring-0 active:bg-[#EA0C5C] active:shadow-lg transition duration-150 ease-in-out my-2 hover:scale-110">Play</button>
+                    <button type="button" className=" inline-block px-6 py-2.5 bg-black text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-[#EA0C5C] hover:shadow-lg focus:bg-[#EA0C5C] focus:shadow-lg focus:outline-none focus:ring-0 active:bg-[#EA0C5C] active:shadow-lg transition duration-150 ease-in-out my-2 hover:scale-110">Add to Playlist</button>
+                    <button type="button" className=" inline-block px-6 py-2.5 bg-black text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-[#EA0C5C] hover:shadow-lg focus:bg-[#EA0C5C] focus:shadow-lg focus:outline-none focus:ring-0 active:bg-[#EA0C5C] active:shadow-lg transition duration-150 ease-in-out my-2 hover:scale-110 flex items-center justify-center"><BsFillHeartFill /></button>
+                  </div>
+                </div>
+              </div>
+            </div>
+            ))}
+          </div>
+          : <div>Loading</div>
+          }
+
+
+          <h4 className="text-xl md:text-lg text-indigo-700 mt-10">Artists</h4>
+          {searchData ?
+          <div className='flex flex-wrap items-centerz justify-center md:grid grid-cols-4 gap-4 items-stretch'>
+            {searchData.artists.results.map((result) => (
+              <div className="flex justify-center m-4 w-full h-full z-[1]">
+              <div className="rounded-lg shadow-lg bg-white max-w-sm">
+                <Link to={result.type === "song" ? "/song" : "/album"} state={result.title} data-mdb-ripple="true" data-mdb-ripple-color="light">
+                  <img className="rounded-t-lg" src={result.image[2].link} alt="Song Cover"/>
+                </Link>
+                <div className="p-6">
+                  <h5 className="text-gray-900 text-xl font-medium mb-2">{result.title}</h5>
+                  <p className="text-gray-700 text-base mb-4">
+                    {result.type}
+                  </p>
+                  <div className='flex justify-between flex-col'>
+                    <button type="button" className=" inline-block px-6 py-2.5 bg-black text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-[#EA0C5C] hover:shadow-lg focus:bg-[#EA0C5C] focus:shadow-lg focus:outline-none focus:ring-0 active:bg-[#EA0C5C] active:shadow-lg transition duration-150 ease-in-out my-2 hover:scale-110">Play</button>
+                    <button type="button" className=" inline-block px-6 py-2.5 bg-black text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-[#EA0C5C] hover:shadow-lg focus:bg-[#EA0C5C] focus:shadow-lg focus:outline-none focus:ring-0 active:bg-[#EA0C5C] active:shadow-lg transition duration-150 ease-in-out my-2 hover:scale-110">Add to Playlist</button>
+                    <button type="button" className=" inline-block px-6 py-2.5 bg-black text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-[#EA0C5C] hover:shadow-lg focus:bg-[#EA0C5C] focus:shadow-lg focus:outline-none focus:ring-0 active:bg-[#EA0C5C] active:shadow-lg transition duration-150 ease-in-out my-2 hover:scale-110 flex items-center justify-center"><BsFillHeartFill /></button>
+                  </div>
+                </div>
+              </div>
+            </div>
+            ))}
+          </div>
+          : <div>Loading</div>
+          }
+
+
         </div>
       </div>
     </div>
